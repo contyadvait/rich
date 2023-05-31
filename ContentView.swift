7478e802e074290904d36c13cb94bc6631345bb7
Advait "Conty" Contractor <advaitthepro@outlook.com>
2023-05-31 11:36:21 +0800

    done

--- ContentView.swift
+++ ContentView.swift
@@ -5,9 +5,16 @@ struct ContentView: View {
         ZStack {
             LinearGradient(colors: [.brown, .black, .brown], startPoint: .topLeading, endPoint: .bottomTrailing)
                 .ignoresSafeArea()
+            Image(systemName: "dollarsign.circle")
+                .resizable(resizingMode: .tile)
+                .padding()
+                .ignoresSafeArea()
             VStack {
-                Text("I AM RICH")
-                    .font(.system(.largeTitle))
+                Text("im rich and no one can stop me")
+                    .font(.system(size: 100))
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .foregroundColor(.black)
             }
         }
     }
